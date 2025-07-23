41d554bbdfe70d0b08ad54beebcc03d0550864a2
Horia612 <hspataru@yahoo.com>
2025-07-24 02:26:46 +0300

    created target

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,1 @@
-//
-//  Main.cpp
-//  cpp_practice
-//
-//  Created by Horia on 23/07/2025.
-//
 
-#include "Main.hpp"
-
-int main()
-{
-    std::cout << "test";
-}
---
+++ PracticeTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PracticeTest
+//
+//  Created by Horia on 24/07/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
