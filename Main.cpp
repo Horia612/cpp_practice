ef0f5afc6dc4d34502445a24ff4eeb9758dc3057
Horia612 <hspataru@yahoo.com>
2025-07-23 15:23:48 +0300

    first Xcode project

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+//
+//  Main.cpp
+//  cpp_practice
+//
+//  Created by Horia on 23/07/2025.
+//
+
+#include "Main.hpp"
+
+int main()
+{
+    std::cout << "test";
+}
---
+++ Main.hpp
@@ -0,0 +1,13 @@
+//
+//  Main.hpp
+//  cpp_practice
+//
+//  Created by Horia on 23/07/2025.
+//
+
+#ifndef Main_hpp
+#define Main_hpp
+
+#include <stdio.h>
+
+#endif /* Main_hpp */
